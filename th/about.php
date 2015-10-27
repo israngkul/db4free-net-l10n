5673a67bcc348bbb3ecf26752e0d202223e7cdc6
Joy Israngkul [Worawit Israngkul] <worawit.isr@mahidol.edu>
2015-10-28 02:29:59 +0700

    แปลเป็นภาษาไทย (ยังไม่สมบูรณ์) 1 of 21 file Translation Completed
    วรวิทย์ อิศรางกูร ณ อยุธยา
    มหาวิทยาลัยมหิดล

---
+++ th/about.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Section: about
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'เกี่ยวกับโครงการ db4free.net',
+
+    'p1' => 'ในส่วนนี้คุณสามารถสร้างฐานข้อมูล แก้ไขข้อมูล และ ลบฐานข้อมูล, modify its data and delete it.',
+
+    'h2' => 'อะไรที่ทำให้เรา "พิเศษกว่า"?',
+
+    'p2' => 'จุดประสงค์ของโครงงานของเราคือ มุ่งที่จะตอบสนองความต้องการของนักพัฒนาโปรแกรม เพื่อการทดสอบ สำหรับ <b>เวอร์ชั่นนี้ของ</b> ฐานข้อมูล MySQL By updating the versions of MySQL and phpMyAdmin frequently, you have for example the opportunity to test your web site with a current MySQL version.',
+
+    'h3' => '<a href="signup.php">Get your own free MySQL database account&nbsp;&raquo;</a>',
+);
+?>
